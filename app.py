aee0eb4a800f67ecc384442cd5a7ca1ca72fd8f4
watchapol Viboonsrisajja <97796118+watchapol-viboonsrisajja@users.noreply.github.com>
2025-02-09 15:07:31 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load model
+model = joblib.load("best_model.pkl")
+
+# Streamlit UI
+st.title("My first ML App (Study on Imbalanced Data Classification by 67xxxx ")
+
+# Input fields
+features = []
+for i in range(11):  # Adjust based on dataset
+    value = st.number_input(f"Feature_{i}", value=0.0)
+    features.append(value)
+
+# Prediction
+if st.button("Predict"):
+    prediction = model.predict([np.array(features)])
+    st.write(f"Predicted Class: {prediction[0]}")
